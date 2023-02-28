bc0dbda65eb4878017f0b3a4becb2bf507bb4f52
dgatowsk <dgatowsk@asu.edu>
2023-02-28 16:29:53 -0700

    Header and Background

--- responsive-architects/js/scripts.js
+++ responsive-architects/js/scripts.js
@@ -1,5 +1,21 @@
 $('.menu-button').click(function(){
     $('nav').toggleClass('open');
 }); 
-// Intialize 
+// Intialize Animte on Scroll
 AOS.init();
+
+// Initialize Jaralax 
+$('.jarallax').jarallax({
+    speed: -1,
+  });
+
+  // Initialize Rellax
+  var rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
+  
